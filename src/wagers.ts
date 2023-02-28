76e495ee381aca9b188e7c7110915651b1f18de9
Josef Leventon <josef.leventon@gmail.com>
2023-02-28 00:10:58 -0500

    Add jobdetail type

--- src/wagers.ts
+++ src/wagers.ts
@@ -12,9 +12,22 @@ interface Price {
   readonly price: number
 }
 
+interface Change {
+  readonly denom: string
+  readonly change: number
+}
+
 export interface Job {
   wager: WagerExport
   prices: [Price, Price]
+}
+
+export interface JobDetail {
+  wager: Job['wager']
+  prev_prices: Job['prices']
+  current_prices: Job['prices']
+  change: [Change, Change]
+  current_winner: NFT | null
 }
 
 export let jobs = new Set<Job>()
